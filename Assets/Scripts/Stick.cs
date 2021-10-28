97f33346a9fddc99cb7dd6b27cb4b649d3ec3261
nvisary <nvisary@gmail.com>
2021-10-28 20:26:50 +0300

    set Ring model to prefab

---
+++ Assets/Scripts/Stick.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Stick : MonoBehaviour
+{
+
+    private void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
